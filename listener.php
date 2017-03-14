31bc8f894eb802db8f3fb627772fe086034e35d4
Jorge Leonel <jleonel@gmail.com>
2017-03-14 11:35:27 -0300

    Update listener.php

--- listener.php
+++ listener.php
@@ -8,7 +8,7 @@
 		<Say>For operating hours, please press 1.</Say>
 		<Say>To speak to  agent, please press 2.</Say>
 	</Gather>
-	<!-- If the person does not typeanything prompt and try one more time. -->
-	<Say>Sorry, I did not obtain your response.</Say>
+	<!-- If the person does not type anything then prompt and try one more time. -->
+	<Say>Sorry, I did not get your input.</Say>
 	<Redirect>listener.php</Redirect>
 </Response>
